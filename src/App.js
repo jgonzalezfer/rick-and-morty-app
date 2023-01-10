2e39562f9b1f1fc4470e38ac4e3404ddf10cf22c
Joel Gonzalez Fernandez <joel.gonzalez5115@gmail.com>
2023-01-10 19:26:23 -0300

    consumir la api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
+import React, {useEffect} from 'react'
 import Navbar from './components/Navbar'
 
-
-fetch("https://rickandmortyapi.com/api/character")
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.log(error))
-
-
-
-
 function App() {
+  const inicialurl = "https://rickandmortyapi.com/api/character"
+
+  const fetchCharacters = (inicialurl) =>{
+    fetch(inicialurl)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+  };
+
+  useEffect (()=>{
+    fetchCharacters (inicialurl);
+  }, [])
+  
   return (
     <Navbar brand="Rick And Morty"/>
   );
